3ead698a9fd5096b8efb6bf49942d931ce3baa9f
姚坤 <yaok@pingansec.com>
2022-01-14 10:25:40 +0800

    测试调试子进程

--- main.c
+++ main.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
+#include <unistd.h>
 
 int main() {
     printf("Hello, World!\n");
+    int a = 1;
+
+    switch (fork()) {
+        case -1:
+            perror("fork error!");
+            break;
+        case 0:
+            a = 2;
+            printf("child process");
+            break;
+        default:
+            a = 3;
+            printf("parent process");
+            break;
+    }
     return 0;
 }
