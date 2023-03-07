fd6d47f6f270f11b1be93007d397e07ff9e9053e
姚坤 <yaok@pingansec.com>
2023-03-07 17:32:25 +0800

    socketpair

--- 进程间通信IPC/03socketpair.c
+++ 进程间通信IPC/03socketpair.c
@@ -1,4 +1,8 @@
 //
+// Created by 姚坤 on 2023/3/7.
+//
+
+//
 // Created by 姚坤 on 2022/2/23.
 //
 
@@ -6,8 +10,10 @@
 
 #include <sys/types.h>          /* See NOTES */
 #include <sys/socket.h>
-
-int socketpair(int domain, int type, int protocol, int sv[2]);
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+// int socketpair(int domain, int type, int protocol, int sv[2]);
 
 // 这个方法可以创建一对关联的套接字 sv[2]，它是使用本机套接字实现的。
 // domain：表示域，在 Linux 下通常取值为 AF_UNIX
@@ -23,3 +29,39 @@ int socketpair(int domain, int type, int protocol, int sv[2]);
 // 通常，在父、子进程通信前，会先调用 socketpair 方法创建这样一组套接字，在调用 fork 方法创建出子进程后，
 // 将会在父进程中关闭 sv[1] 套接字，仅使用 sv[0] 套接字用于向子进程发送数据以及接收子进程发送来的数据
 // 而在子进程中则关闭 sv[0] 套接字，仅使用 sv[1] 套接字既可以接收父进程发送来的数据，也可以向父进程发送数据
+
+int main(){
+    int socket_vector[2];
+    int r = socketpair(AF_UNIX, SOCK_STREAM, 0, socket_vector);
+    if(r == -1){
+        perror("");
+        exit(1);
+    }
+    printf("读：%d,写：%d\n", socket_vector[0], socket_vector[1]);
+
+    int pid = fork();
+    if(pid == 0){// child write
+        close(socket_vector[0]);
+
+        for (int i = 0; i < 10; ++i) {
+            sleep(1);
+            write(socket_vector[1], &i, sizeof(i));
+        }
+        close(socket_vector[1]);
+
+    }else{// parent read
+        close(socket_vector[1]);
+        int num = 0;
+        while (1){
+            r = read(socket_vector[0],&num,sizeof(num));
+            if(r <= 0){
+                perror("");
+                break;
+            }
+            printf("recv: %d\n", num);
+        }
+        close(socket_vector[0]);
+        wait(NULL);
+    }
+    return 0;
+}
