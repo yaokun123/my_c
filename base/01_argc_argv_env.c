de9afdc596ab76dc5b55fbc7c4b004ece5a74a8a
姚坤 <yaok@pingansec.com>
2023-03-08 10:04:17 +0800

    const

---
+++ base/01_argc_argv_env.c
@@ -0,0 +1,21 @@
+//
+// Created by 姚坤 on 2023/3/8.
+//
+
+#include <stdio.h>
+
+int main(int argc, char *argv[], char *env[]){
+    printf("参数个数：%d\n", argc);
+
+    while (*argv){
+        printf("%s ", *argv++);
+    }
+    printf("\n");
+
+    while (*env){
+        printf("%s ", *env++);
+    }
+    printf("\n");
+    return 0;
+}
+
